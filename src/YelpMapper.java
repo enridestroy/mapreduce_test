a4a31f42b2c83a156537d958924615d310ddc0e8
Enridestroy <contact@enridestroy.fr>
2015-02-26 14:43:27 -0500

    Signed-off-by: Enridestroy <contact@enridestroy.fr>

--- src/YelpMapper.java
+++ src/YelpMapper.java
@@ -6,12 +6,12 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-import com.json.exceptions.JSONParsingException;
 import com.json.parsers.JSONParser;
 import com.json.parsers.JsonParserFactory;
+import com.json.exceptions.JSONParsingException;
 
 
-public class YelpMapper extends Mapper<Text, Text, Text, IntWritable>{
+public class YelpMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
 	
 	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 		/**
@@ -26,14 +26,15 @@ public class YelpMapper extends Mapper<Text, Text, Text, IntWritable>{
         parser.setValidating(false);
         
         Map<String, Object> jsonMap = null;
+        
         try{
         	jsonMap = parser.parseJson(line);
         }
         catch(JSONParsingException e){
-        	
+        	System.out.println("erreur du parsing json.");
         }
-        
-        if(jsonMap!=null){
+                
+        if(jsonMap!=null && jsonMap.get("type")!=null && jsonMap.get("type").equals("review")){
         	String user = (String) jsonMap.get("user_id");
         	context.write(new Text(user), new IntWritable(1));//on ajoute un item au user en question
         }		
